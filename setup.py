1a83e3e3dd24bda3e2155782edf663f77e23b971
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2025-08-03 11:42:20 +0300

    build: add extra info in `setup.py`

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup, find_packages
 
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.readlines()
 
 setup(
     name='colorME',
     version='0.1.0',
     description='A simple coloring util for better output visibility',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Abdulrahman Khaled',
+    license='Apache-2.0',
     packages=find_packages(),
     install_requires=['termcolor'],
     classifiers=[
