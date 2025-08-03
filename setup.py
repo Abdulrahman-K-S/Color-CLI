72b5ed3d70cec2ee9336aca250298a2f8b40192e
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2025-08-03 12:30:43 +0300

    update: package info

--- setup.py
+++ setup.py
@@ -6,11 +6,13 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='color_cli',
-    version='0.1.0',
+    version='0.1.1',
     description='A simple coloring util for better output visibility',
     long_description=long_description,
     long_description_content_type='text/markdown',
+    url='https://github.com/Abdulrahman-K-S/Color-CLI',
     author='Abdulrahman Khaled',
+    author_email='AK-Salah@outlook.com',
     license='Apache-2.0',
     packages=find_packages(),
     install_requires=['termcolor'],
